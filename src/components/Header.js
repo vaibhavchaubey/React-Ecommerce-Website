77cd436a0efc22deb3b8e92f1f50b729a00fe9a9
Vaibhav <vaibhavchaubey8802@gmail.com>
2023-06-18 15:27:38 +0530

    Added Header with Navbar

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import Nav from './Nav';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <MainHeader>
+      <NavLink to="/">
+        <img src="./images/logo.png" alt="my logo img" />
+      </NavLink>
+      <Nav />
+    </MainHeader>
+  );
+};
+
+const MainHeader = styled.header`
+  padding: 0 4.8rem;
+  height: 10rem;
+  background-color: ${({ theme }) => theme.colors.bg};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+
+  .logo {
+    height: 5rem;
+  }
+`;
+
+export default Header;
