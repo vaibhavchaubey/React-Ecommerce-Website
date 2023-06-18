c4285e329a69ea935eeccc8c56b7e007ebc3479d
Vaibhav <vaibhavchaubey8802@gmail.com>
2023-06-18 18:23:56 +0530

    Added Services & Trusted Section

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import HeroSection from './components/HeroSection';
+import Services from './components/Services';
+import Trusted from './components/Trusted';
 
 const Home = () => {
   const data = {
-    name: "thapa store"
-  }
-  return <HeroSection myData={data} />;
+    name: 'thapa store',
+  };
+
+  return (
+    <>
+      <HeroSection myData={data} />
+      <Services />
+      <Trusted />
+    </>
+  );
 };
 
 export default Home;
+
+
