1315117c8b2fbbf187dee3e3b09a6500e29c2e03
Vaibhav <vaibhavchaubey8802@gmail.com>
2023-06-18 17:30:09 +0530

    Added HeroSection

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -85,7 +85,7 @@ li {
 ${"" /* resuable code section  */}
 
 .container {
-  max-width: 120rem;
+  max-width: 100rem;
   margin: 0 auto;
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
+import HeroSection from './components/HeroSection';
 
 const Home = () => {
-  return <Wrapper className="test">Home</Wrapper>;
+  const data = {
+    name: "thapa store"
+  }
+  return <HeroSection myData={data} />;
 };
-
-const Wrapper = styled.section`
-  height: 100vh;
-  background-color: ${({ theme }) => theme.colors.bg};
-`;
 
 export default Home;
