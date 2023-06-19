1223bcfb2a7ccc5134108a5c4707f5dec4a7662c
Vaibhav <vaibhavchaubey8802@gmail.com>
2023-06-19 10:48:36 +0530

    Added Contact From

--- src/Contact.js
+++ src/Contact.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const Contact = () => {
   const Wrapper = styled.section`
@@ -17,7 +17,7 @@ const Contact = () => {
           flex-direction: column;
           gap: 3rem;
 
-          input[type="submit"] {
+          input[type='submit'] {
             cursor: pointer;
             transition: all 0.2s;
 
@@ -33,7 +33,53 @@ const Contact = () => {
     }
   `;
 
-  return <Wrapper><h1>Contact</h1></Wrapper>;
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Contact Page</h2>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235526.94958439682!2d75.69903299005296!3d22.724204999707233!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3962fcad1b410ddb%3A0x96ec4da356240f4!2sIndore%2C%20Madhya%20Pradesh!5e0!3m2!1sen!2sin!4v1687147679044!5m2!1sen!2sin"
+        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+      <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/mlekqozl"
+            method="POST"
+            className="contact-inputs"
+          >
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              required
+              autoComplete="off"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="Email"
+              required
+              autoComplete="off"
+            />
+            <textarea
+              name="Message"
+              cols="30"
+              rows="10"
+              placeholder="Enter your message"
+              required
+              autoComplete="off"
+            ></textarea>
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Contact;
