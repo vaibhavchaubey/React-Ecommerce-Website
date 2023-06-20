39374ce625c7d9e9a87f99bbd7cc377126129060
Vaibhav <vaibhavchaubey8802@gmail.com>
2023-06-20 09:00:49 +0530

    Demo

--- src/About.js
+++ src/About.js
@@ -1,11 +1,17 @@
-import React from 'react';
 import HeroSection from './components/HeroSection';
+import { useProductContext } from './context/productcontext';
 
 const About = () => {
+  const {myName} = useProductContext();
   const data = {
-    name: "Thapa Ecommerce"
-  }
-  return <HeroSection myData={data} />;
+    name: 'Thapa Ecommerce',
+  };
+  return (
+    <>
+      {myName}
+      <HeroSection myData={data} />;
+    </>
+  );
 };
 
 export default About;
---
+++ src/context/productcontext.js
@@ -0,0 +1,16 @@
+import { createContext, useContext } from "react";
+
+const AppContext = createContext()
+
+const AppProvider = ({children}) =>{
+    return <AppContext.Provider value={{myName: "vaibhav"}}>
+        {children}
+    </AppContext.Provider>
+}
+
+// custom hook
+const useProductContext = ()=>{
+    return useContext(AppContext);
+}
+
+export {AppProvider, AppContext, useProductContext}
