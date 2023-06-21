dd9497348860475403dc1eb490c5e2c1930d8f4c
Vaibhav <vaibhavchaubey786@gmail.com>
2023-06-21 17:42:06 +0530

    Added Feature Section of Home Page

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HeroSection from './components/HeroSection';
 import Services from './components/Services';
 import Trusted from './components/Trusted';
+import FeaturedProduct from './components/FeaturedProduct';
 
 const Home = () => {
   const data = {
@@ -11,6 +12,7 @@ const Home = () => {
   return (
     <>
       <HeroSection myData={data} />
+      <FeaturedProduct />
       <Services />
       <Trusted />
     </>
