c0e626a19407edb325577cf120a0f4500e2d12a0
Vaibhav <vaibhavchaubey786@gmail.com>
2023-06-25 14:00:11 +0530

    Products Page Sorting completed

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -1,6 +1,6 @@
-import { createContext, useContext, useEffect, useReducer } from 'react';
-import { useProductContext } from './productcontext';
-import reducer from '../reducer/filterReducer';
+import { createContext, useContext, useReducer, useEffect } from "react";
+import { useProductContext } from "./productcontext";
+import reducer from "../reducer/filterReducer";
 
 const FilterContext = createContext();
 
@@ -8,29 +8,72 @@ const initialState = {
   filter_products: [],
   all_products: [],
   grid_view: true,
+  sorting_value: "lowest",
+  filters: {
+    text: "",
+    category: "all",
+    company: "all",
+  },
 };
 
 export const FilterContextProvider = ({ children }) => {
   const { products } = useProductContext();
+
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  /* to set the grid veiw */
+  // to set the grid view
   const setGridView = () => {
-    return dispatch({ type: 'SET_GRIDVIEW' });
+    return dispatch({ type: "SET_GRID_VIEW" });
   };
 
+  // to set the list view
+  const setListView = () => {
+    return dispatch({ type: "SET_LIST_VIEW" });
+  };
+
+  // sorting function
+  const sorting = (event) => {
+    let userValue = event.target.value;
+    dispatch({ type: "GET_SORT_VALUE", payload: userValue });
+  };
+
+  // update the filter values
+  const updateFilterValue = (event) => {
+    let name = event.target.name;
+    let value = event.target.value;
+
+    if (name === "company") {
+      value = event.target.value;
+    }
+
+    return dispatch({ type: "UPDATE_FILTERS_VALUE", payload: { name, value } });
+  };
+
+  // to sort the product
   useEffect(() => {
-    dispatch({ type: 'LOAD_FILTER_PRODUCTS', payload: products });
+    // dispatch({ type: "FILTER_PRODUCTS" });
+    dispatch({ type: "SORTING_PRODUCTS" });
+  }, [products, state.sorting_value]);
+
+  // to load all the products for grid and list view
+  useEffect(() => {
+    dispatch({ type: "LOAD_FILTER_PRODUCTS", payload: products });
   }, [products]);
 
   return (
-    <FilterContext.Provider value={{ ...state, setGridView }}>
+    <FilterContext.Provider
+      value={{
+        ...state,
+        setGridView,
+        setListView,
+        sorting,
+        updateFilterValue,
+      }}>
       {children}
     </FilterContext.Provider>
   );
 };
 
-// custom hook
 export const useFilterContext = () => {
   return useContext(FilterContext);
 };
