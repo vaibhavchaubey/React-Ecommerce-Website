6595a1739480d65457768edf0674fd208e4c4f3f
Vaibhav <vaibhavchaubey786@gmail.com>
2023-06-30 17:38:38 +0530

    Website Frontend done

--- src/Cart.js
+++ src/Cart.js
@@ -1,13 +1,26 @@
 import styled from "styled-components";
 import { useCartContext } from "./context/cart_context";
 import CartItem from "./components/CartItem";
+import { NavLink } from "react-router-dom";
+import { Button } from "./styles/Button";
+import FormatPrice from "./Helpers/FormatPrice";
 
 const Cart = () => {
-  const { cart } = useCartContext();
+  const { cart, clearCart, total_price, shipping_fee } = useCartContext();
+  // console.log("🚀 ~ file: Cart.js ~ line 6 ~ Cart ~ cart", cart);
+
+  if (cart.length === 0) {
+    return (
+      <EmptyDiv>
+        <h3>No Cart in Item </h3>
+      </EmptyDiv>
+    );
+  }
+
   return (
     <Wrapper>
       <div className="container">
-        <div className="cart-heading grid grid-five-column">
+        <div className="cart_heading grid grid-five-column">
           <p>Item</p>
           <p className="cart-hide">Price</p>
           <p>Quantity</p>
@@ -15,24 +28,66 @@ const Cart = () => {
           <p>Remove</p>
         </div>
         <hr />
-
         <div className="cart-item">
           {cart.map((curElem) => {
             return <CartItem key={curElem.id} {...curElem} />;
           })}
+        </div>
+        <hr />
+        <div className="cart-two-button">
+          <NavLink to="/products">
+            <Button> continue Shopping </Button>
+          </NavLink>
+          <Button className="btn btn-clear" onClick={clearCart}>
+            clear cart
+          </Button>
+        </div>
+
+        {/* order total_amount */}
+        <div className="order-total--amount">
+          <div className="order-total--subdata">
+            <div>
+              <p>subtotal:</p>
+              <p>
+                <FormatPrice price={total_price} />
+              </p>
+            </div>
+            <div>
+              <p>shipping fee:</p>
+              <p>
+                <FormatPrice price={shipping_fee} />
+              </p>
+            </div>
+            <hr />
+            <div>
+              <p>order total:</p>
+              <p>
+                <FormatPrice price={shipping_fee + total_price} />
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </Wrapper>
   );
 };
 
+const EmptyDiv = styled.div`
+  display: grid;
+  place-items: center;
+  height: 50vh;
+  h3 {
+    font-size: 4.2rem;
+    text-transform: capitalize;
+    font-weight: 300;
+  }
+`;
+
 const Wrapper = styled.section`
   padding: 9rem 0;
-
   .grid-four-column {
     grid-template-columns: repeat(4, 1fr);
   }
-
   .grid-five-column {
     grid-template-columns: repeat(4, 1fr) 0.3fr;
     text-align: center;
@@ -51,14 +106,12 @@ const Wrapper = styled.section`
     flex-direction: column;
     gap: 3.2rem;
   }
-
   .cart-user--profile {
     display: flex;
     justify-content: flex-start;
     align-items: center;
     gap: 1.2rem;
     margin-bottom: 5.4rem;
-
     img {
       width: 8rem;
       height: 8rem;
@@ -85,57 +138,47 @@ const Wrapper = styled.section`
       object-fit: contain;
       color: transparent;
     }
-
     .color-div {
       display: flex;
       align-items: center;
       justify-content: flex-start;
       gap: 1rem;
-
       .color-style {
         width: 1.4rem;
         height: 1.4rem;
-
         border-radius: 50%;
       }
     }
   }
-
   .cart-two-button {
     margin-top: 2rem;
     display: flex;
     justify-content: space-between;
-
     .btn-clear {
       background-color: #e74c3c;
     }
   }
-
   .amount-toggle {
     display: flex;
     justify-content: center;
     align-items: center;
     gap: 2.4rem;
     font-size: 1.4rem;
-
     button {
       border: none;
       background-color: #fff;
       cursor: pointer;
     }
-
     .amount-style {
       font-size: 2.4rem;
       color: ${({ theme }) => theme.colors.btn};
     }
   }
-
   .remove_icon {
     font-size: 1.6rem;
     color: #e74c3c;
     cursor: pointer;
   }
-
   .order-total--amount {
     width: 100%;
     margin: 4.8rem 0;
@@ -144,7 +187,6 @@ const Wrapper = styled.section`
     flex-direction: column;
     justify-content: flex-end;
     align-items: flex-end;
-
     .order-total--subdata {
       border: 0.1rem solid #f0f0f0;
       display: flex;
@@ -157,17 +199,14 @@ const Wrapper = styled.section`
       gap: 3.2rem;
       justify-content: space-between;
     }
-
     div:last-child {
       background-color: #fafafa;
     }
-
     div p:last-child {
       font-weight: bold;
       color: ${({ theme }) => theme.colors.heading};
     }
   }
-
   @media (max-width: ${({ theme }) => theme.media.mobile}) {
     .grid-five-column {
       grid-template-columns: 1.5fr 1fr 0.5fr;
@@ -175,20 +214,17 @@ const Wrapper = styled.section`
     .cart-hide {
       display: none;
     }
-
     .cart-two-button {
       margin-top: 2rem;
       display: flex;
       justify-content: space-between;
       gap: 2.2rem;
     }
-
     .order-total--amount {
       width: 100%;
       text-transform: capitalize;
       justify-content: flex-start;
       align-items: flex-start;
-
       .order-total--subdata {
         width: 100%;
         border: 0.1rem solid #f0f0f0;
